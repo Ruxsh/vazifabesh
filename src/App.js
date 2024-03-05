a753ed6460be61744b52d6cfdab5b75875c227be
Black-monster1 <otabekruhshona@gmail.com>
2024-03-05 19:23:54 +0500

    what

--- src/App.js
+++ src/App.js
@@ -4,7 +4,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>what happent</h1>
+      <header className='headerbir'>
+        <div>
+          <p>Owerview Page</p>
+        </div>
+        <div className='box'>
+          <p>Monitor</p>
+          <p>Ipad</p>
+          <p>Phone</p>
+        </div>
+        <div>
+          <h6>git</h6>
+          <button>Free Download</button>
+        </div>
+      </header>
+      <main>
+        <div>
+          <h4>Start Bootstrap</h4>
+        </div>
+        <div>
+          <span>Home</span>
+          <span>About</span>
+          <span>Contact</span>
+          <span>Blog</span>
+        </div>
+      </main>
+
+      <aside>
+        <div>
+          <h1>Welcome to Blog Home!</h1>
+          <p>A Bootstrap 5 starter for your next blog homepage</p>
+        </div>
+      </aside>
     </div>
   );
 }
