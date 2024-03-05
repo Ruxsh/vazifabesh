8ee8d4a3be549933ae674752c8b7d73e928bdbb6
Black-monster1 <otabekruhshona@gmail.com>
2024-03-05 19:10:54 +0500

    yourself

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import monitor from './monitor.jpg'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>good lock</h1>
-        <img src={monitor} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>what happent</h1>
     </div>
   );
 }
